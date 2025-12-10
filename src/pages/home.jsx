17fea0b1fa147f2aee6a8833ec6ee1bedbaeb9ae
pooja verma <vepooja47@gmail.com>
2025-12-10 21:08:16 +0530

    changes

--- src/pages/home.jsx
+++ src/pages/home.jsx
@@ -17,7 +17,7 @@ const Home = () => {
             Hi, I'm <span className="text-[#008B8B]">Pooja verma</span>
           </h1>
           <p className="text-gray-600 text-lg">
-            I’m a Full Stack Developer who loves building beautiful and
+            I’m a Web Developer who loves building beautiful and
             functional web applications. Let’s build something amazing!
           </p>
           <Button className="rounded-full px-6 py-2 text-white bg-[#008B8B]hover:bg-blue-700 transition">
@@ -30,7 +30,7 @@ const Home = () => {
           <img
             src={Profile}
             alt="Coding Illustration"
-           className="rounded-full shadow-lg w-60 h-60 md:w-64 md:h-64 object-cover image-full aspect-square "
+           className="rounded-full shadow-lg w-60 h-60 md:w-64 md:h-64 object-cover object-center aspect-square"
           />
         </div>
       </div>
